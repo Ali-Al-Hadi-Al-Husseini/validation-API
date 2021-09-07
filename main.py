90bb157a6206eb1ff5d76d626c1515c76b0b4c1a
Ali-Al-Hadi-Al-Husseini <77203019+Ali-Al-Hadi-Al-Husseini@users.noreply.github.com>
2021-09-07 10:55:23 +0300

    fixed email validtion added url shortner

--- main.py
+++ main.py
@@ -1,39 +1,51 @@
-from flask import Flask
-from re import search 
-from socket import gethostname
-from smtplib import SMTP
+from flask import Flask , redirect
+from re import search
+import smtplib
+from os import popen
 import dns.resolver
+from flask_sqlalchemy import SQLAlchemy
+from string import ascii_letters , digits
+from random import choice
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///myDB.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+def find_best_mx(result):
+    mx_score   = [result[8], result[17],result[26],result[35],result[44]]
+    mx_records = [result[12],result[21],result[30],result[39], result[48]]
+    smallest = float('inf')
+    for i in range(len(mx_score)):
+        if smallest > 1:
+            smallest = i
+
+    return mx_records[smallest]
 
 @app.route('/email/<email>')
 def email_validator(email):
-    email_regex = '^[a-zA-Z0-9]+[\._]?[a-zA-Z0-9]+[@]\w+[.]\w{2,3}$'
+    email_regex = "^[a-zA-Z0-9]+[\._]?[a-zA-Z0-9]+[@]\w+[.]\w{2,3}$"
 
     if search(email_regex, email):
-            domain_name = email.split('@')[1]
-
-        # try:
+        domain_name = email.split('@')[1]
+        try:
             records = dns.resolver.resolve(domain_name, 'MX')
-            mxrecord = records[0].exchange
-            mxrecord = str(mxrecord)
+            mxRecord = records[0].exchange
+            mxRecord = str(mxRecord)
 
-            host = gethostname()
+            # SMTP lib setup
+            server = smtplib.SMTP()
 
-            server = SMTP()
-            server.set_debuglevel(0)
-            print(domain_name)
-            server.connect(mxrecord)
-            server.helo(host)
-            server.mail(email)
+            fromAddress = 'just_a_place_holder@domain.com'
+
+            # SMTP Conversation
+            server.connect(mxRecord)
+            server.helo(server.local_hostname)  ### server.local_hostname(Get local server hostname)
+            server.mail(fromAddress)
             code, message = server.rcpt(str(email))
             server.quit()
-
-            return code
-        #
-        # except:
-        #     return 'False'
+        finally:
+            return str(code == 250)
 
     else:
         return "False"
@@ -55,14 +67,78 @@ def card_validator(number):
 
     for idx in range(1, len(numbers_list), 2):
         new_num = numbers_list[idx] * 2
-        print(numbers_list[idx])
-        if new_num > 9:
+
+        if new_num <= 9:
+            pass
+        else:
             new_num = sum_of_num(new_num)
 
         numbers_list[idx] = new_num
-    print(numbers_list)
+
     return str(sum(numbers_list) % 10 == 0)
 
+@app.route("/domain/<domain_name>")
+def domain_validator(domain_name):
+    response = popen(f"ping {domain_name}").read()
+
+    if "Received = 4" in response:
+        return "True"
+    else:
+        return "False"
+
+
+# @app.route("ssl/<ssl_cert>")
+# def ssl_validator(ssl_cert):
+#     pass
+
+def generate_random_key():
+    return ''.join(choice(ascii_letters + digits) for i in range(8))
+
+def check_if_exists(url):
+    url = Url.query.filter_by(shortened_url=url).first()
+    result = url != None
+    return result
+
+# model for url shortner
+class Url(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    shortened_url = db.Column(db.String(100), unique=True)
+    url = db.Column(db.String(1000))
+
+    def __init__(self, url, shortened):
+        self.url = str(url)
+        self.shortened_url = str(shortened)
+
+
+@app.route("/shorten/<url>/<shortened_version>")
+@app.route("/shorten/<path:url>")
+def shorten_url(url, shortened_version=None):
+    if domain_validator(url) == "False":
+        return "domain doesn't exist"
+    if shortened_version is not None:
+        shortened_version = str(shortened_version)
+        does_exist = check_if_exists(shortened_version)
+        if does_exist:
+            return "chosen words already exists"
+        else:
+            new_url = Url(url, shortened_version)
+            key = shortened_version
+    else:
+        key = generate_random_key()
+        # while check_if_exists(key):
+        #     key = generate_random_key()
+        new_url = Url(url, key)
+    print(new_url.url)
+    print(new_url.shortened_url)
+    db.session.add(new_url)
+    db.session.commit()
+    return key
+
+@app.route("/<key>")
+def visit_shortend(key):
+    URL = Url.query.filter_by(shortened_url=str(key)).first()
+    return redirect("https://" + URL.url)
 
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True)
